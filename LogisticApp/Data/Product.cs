2eedcb75b60f411190c0584d9f17b21965ed8ce2
Toni Peise <toni.peise@gmx.net>
2022-02-18 02:25:06 +0100

    Add project files.

---
+++ LogisticApp/Data/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace LogisticApp.Data
+{
+    public class Product
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
---
+++ LogisticApp/Data/ProductService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LogisticApp.Data
+{
+    public class ProductService
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Product>> GetAllProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+        public async Task<Product> GetProduct(int id)
+        {
+            Product productFromDb = await _context.Products.SingleOrDefaultAsync(p => p.Id == id);
+            return productFromDb;
+        }
+        public async Task<bool> InsertProducts(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> UpdateProducts(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> DeleteProducts(Product product)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+    }
+}
